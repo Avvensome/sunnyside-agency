53aea9d01ca6536d06bb6a405602ab5cb3ebd3bf
Patryk <p.zawistowski190798@gmail.com>
2023-09-09 15:17:14 +0200

    Set up header container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 function App() {
-	return <div className="App"></div>;
+	return (
+		<header>
+			<img
+				className="image-header"
+				src="/images/desktop/image-header.jpg"
+				alt="main iamge header"
+			/>
+			<div className="header-container">
+				<p>sunnyside</p>
+				<nav>
+					<button>About</button>
+					<button>Services</button>
+					<button>Projects</button>
+					<button>CONTACT</button>
+				</nav>
+			</div>
+			<div className="header-title">
+				<h1>we are creatives</h1>
+				<img
+					className="arrow-down-icon"
+					src="/images/icons/icon-arrow-down.svg"
+					alt="arrow down icon"
+				/>
+			</div>
+		</header>
+	);
 }
 
 export default App;
