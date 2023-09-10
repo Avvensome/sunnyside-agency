aee80c2948b63cecf3d7f0d77af3a02b36b09047
Patryk <p.zawistowski190798@gmail.com>
2023-09-10 12:13:16 +0200

    End desktop header section

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 function App() {
 	return (
-		<header>
-			<img
-				className="image-header"
-				src="/images/desktop/image-header.jpg"
-				alt="main iamge header"
-			/>
-			<div className="header-container">
-				<p>sunnyside</p>
-				<nav>
-					<button>About</button>
-					<button>Services</button>
-					<button>Projects</button>
-					<button>CONTACT</button>
-				</nav>
-			</div>
-			<div className="header-title">
-				<h1>we are creatives</h1>
-				<img
-					className="arrow-down-icon"
-					src="/images/icons/icon-arrow-down.svg"
-					alt="arrow down icon"
-				/>
-			</div>
-		</header>
+		<>
+			<header>
+				<picture>
+					<source
+						className="image-header"
+						media="(min-width: 1440px)"
+						srcset="images/desktop/image-header.jpg"
+						type="image/jpeg"
+					/>
+					<source
+						className="image-header"
+						media="(max-width: 375px)"
+						srcset="images/mobile/image-header.jpg"
+						type="image/jpeg"
+					/>
+
+					<img
+						className="image-header"
+						src="/images/desktop/image-header.jpg"
+						alt="main iamge header"
+					/>
+				</picture>
+				<div className="header-container">
+					<p>sunnyside</p>
+					<nav>
+						<button>About</button>
+						<button>Services</button>
+						<button>Projects</button>
+						<button className="contact-btn">CONTACT</button>
+					</nav>
+				</div>
+				<div className="header-title">
+					<h1>we are creatives</h1>
+					<img
+						className="arrow-down-icon"
+						src="/images/icons/icon-arrow-down.svg"
+						alt="arrow down icon"
+					/>
+				</div>
+			</header>
+		</>
 	);
 }
 
