7beb99c853b426b1cbecbd11621de5b4ce37ea34
Patryk <p.zawistowski190798@gmail.com>
2023-09-12 23:01:32 +0200

    End mobile version app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,92 @@
+function PictureWithOptionalText({
+	srcDesktop,
+	srcMobile,
+	alt = "picture",
+	className = "",
+	titleText = "",
+	descriptionText = "",
+	textColor = "",
+}) {
+	return (
+		<picture className="picture-container">
+			<source
+				media="(min-width: 1440px)"
+				srcset={srcDesktop}
+				type="image/jpeg"
+			/>
+			<source
+				media="(min-width: 375px)"
+				srcset={srcMobile}
+				type="image/jpeg"
+			/>
+
+			<img className={className} src={srcDesktop} alt={alt} />
+			{titleText && (
+				<div
+					className="picture-text-container"
+					style={{ color: textColor }}
+				>
+					<h2>{titleText}</h2>
+					<p>{descriptionText}</p>
+				</div>
+			)}
+		</picture>
+	);
+}
+function TextComponent({
+	textTitle,
+	textDescription,
+	textButton = "learn more",
+	borderColor,
+}) {
+	return (
+		<div className="info-container">
+			<h2>{textTitle}</h2>
+			<p>{textDescription}</p>
+			<span style={{ borderColor: borderColor }}>{textButton}</span>
+		</div>
+	);
+}
+function ClientTestimonial({
+	imgSrc,
+	imgAlt,
+	personTestimonal,
+	personName,
+	personJobPosition,
+}) {
+	return (
+		<div>
+			<img src={imgSrc} alt={imgAlt} />
+			<p>{personTestimonal}</p>
+			<h4>{personName}</h4>
+			<h5>{personJobPosition}</h5>
+		</div>
+	);
+}
+
 function App() {
 	return (
 		<>
 			<header>
 				<PictureWithOptionalText
+					className={`image-header`}
 					srcDesktop={"images/desktop/image-header.jpg"}
 					srcMobile={"images/mobile/image-header.jpg"}
-					className={`image-header`}
 				/>
 				<div className="header-container">
 					<p>sunnyside</p>
-					<nav>
+					<nav className="desktop-nav">
 						<button>About</button>
 						<button>Services</button>
 						<button>Projects</button>
 						<button className="contact-btn">CONTACT</button>
+					</nav>
+					<nav className="mobile-nav">
+						<img
+							className="hamburger-menu"
+							src="images/icons/icon-hamburger.svg"
+							alt="hamburger icon"
+						/>
 					</nav>
 				</div>
 				<div className="header-title">
@@ -32,6 +105,7 @@ function App() {
 						helping brands grow fast. Enpage your clients through
 						compelling visuals that do most of the marketing for
 						you.`}
+					borderColor={`hsl(51, 100%, 49%)`}
 				/>
 				<PictureWithOptionalText
 					srcDesktop={"images/desktop/image-transform.jpg"}
@@ -46,6 +120,7 @@ function App() {
 					textDescription={`Using a collaborative formula of designers, researchers,
 						photographers, videographeres, and copywriters, we'll
 						build and extend your brand in digital places`}
+					borderColor={"hsl(7, 99%, 70%)"}
 				/>
 				<PictureWithOptionalText
 					srcDesktop={`images/desktop/image-graphic-design.jpg`}
@@ -112,7 +187,7 @@ function App() {
 				/>
 				<PictureWithOptionalText
 					srcDesktop={`images/desktop/image-gallery-sugarcubes.jpg`}
-					srcMobile={`images/mobile/image-gallery-sugarcubes.jpg`}
+					srcMobile={"images/mobile/image-gallery-sugar-cubes.jpg"}
 					alt="sugar cubes"
 				/>
 			</section>
@@ -148,70 +223,4 @@ function App() {
 	);
 }
 
-function PictureWithOptionalText({
-	srcDesktop,
-	srcMobile,
-	alt = "picture",
-	className = "",
-	titleText = "",
-	descriptionText = "",
-	textColor = "",
-}) {
-	return (
-		<picture className="picture-container">
-			<source
-				className={className}
-				media="(min-width: 1440px)"
-				srcset={srcDesktop}
-				type="image/jpeg"
-			/>
-			<source
-				className={className}
-				media="(max-width: 375px)"
-				srcset={srcMobile}
-				type="image/jpeg"
-			/>
-
-			<img className={className} src={srcDesktop} alt={alt} />
-			{titleText && (
-				<div
-					className="picture-text-container"
-					style={{ color: textColor }}
-				>
-					<h2>{titleText}</h2>
-					<p>{descriptionText}</p>
-				</div>
-			)}
-		</picture>
-	);
-}
-function TextComponent({
-	textTitle,
-	textDescription,
-	textButton = "learn more",
-}) {
-	return (
-		<div className="info-container">
-			<h2>{textTitle}</h2>
-			<p>{textDescription}</p>
-			<span>{textButton}</span>
-		</div>
-	);
-}
-function ClientTestimonial({
-	imgSrc,
-	imgAlt,
-	personTestimonal,
-	personName,
-	personJobPosition,
-}) {
-	return (
-		<div>
-			<img src={imgSrc} alt={imgAlt} />
-			<p>{personTestimonal}</p>
-			<h4>{personName}</h4>
-			<h5>{personJobPosition}</h5>
-		</div>
-	);
-}
 export default App;
